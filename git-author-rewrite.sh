867a6e87fff0c657120dcb521536610a00a06720
CodeBag <codemaven0923@gmail.com>
2024-05-21 00:10:34 -0700

    change readme

---
+++ git-author-rewrite.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="adrianhajdin00@gmail.com"
+CORRECT_NAME="CodeBag0923"
+CORRECT_EMAIL="codemaven0923@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
